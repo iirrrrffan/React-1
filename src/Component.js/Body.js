563593a9614bb344126df3be70dd6ed5ecaafdca
iirrrrffan <irfanali376246@gmail.com>
2023-10-20 11:37:38 +0530

    first commit

---
+++ src/Component.js/Body.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Body(){
+    return (
+        <div>
+            <ol>
+                <li>Full Name: Lionel Andrés Messi </li>
+                <li>Date of Birth: June 24, 1987 </li>
+                <li> Place of Birth: Rosario, Santa Fe, Argentina </li>
+                <li>Position: Forward </li>
+                
+                      </ol>
+            <p>Lionel Messi has had an illustrious career and is known for his extraordinary skills,
+                 incredible dribbling ability, vision, and goal-scoring prowess.
+                  He has achieved numerous accolades and records throughout his career,
+                 making him one of the most decorated footballers of all time.</p>
+        </div>
+    )
+}
+export default Body
---
+++ src/Component.js/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header(){
+    return(
+        <div>
+            <h1>Lionel Messi</h1>
+        </div>
+    )
+}
+export default Header
